dd65e82949dde3b968dfeda4a9fe1c68eba0604e
Gabriel Araujo <gabriel_scf@hotmail.com>
2023-04-28 23:43:32 -0300

    Reorganize benchmark antibody definitions.

--- pmc_antibody/benchmark/target.py
+++ pmc_antibody/benchmark/target.py
@@ -60,26 +60,32 @@ def benchmark_antibodies():
         AntibodyInformation("11-0041-82", "GK1.5", invitrogen, "CD4"),
         AntibodyInformation("ab183685", "EPR19514", "Abcam", "CD4"),
         AntibodyInformation("100401", "GK1.5", "BioLegend", "CD4"),
+
+        # 05;
         AntibodyInformation("ab133616", "EPR6855", "Abcam", "CD4"),
         AntibodyInformation("AF1828", None, "R&D Systems", "TREM2"),
         AntibodyInformation("BAF1828", None, "R&D Systems", "TREM2"),
-
-        # 08;
         AntibodyInformation("MA5-12542", "5C5.F8.C7", invitrogen, "ACTA1"),
+
+        # 09;
         AntibodyInformation("AB5054", None, "MilliporeSigma", "CALB2"),
         AntibodyInformation("130-095-822", "ACSA-1", "Miltenyi Biotec", "GLAST"),  # Target??
         AntibodyInformation("60161.2", "2.4G2", "STEMCELL Technologies", "Fcgr3"),
         AntibodyInformation("9102", None, "Cell Signaling Technology", "MAPK"),  # Target alternative names??
+
+        # 13;
         AntibodyInformation("3661", None, "Cell Signaling Technology", "ACACA"),
         AntibodyInformation("BA-2001", None, "Vector Laboratories", "Anti-Mouse"),
         AntibodyInformation("3533", None, "BioVision", "Collagenase 3 (Rat)"),  # Target gene name: MMP13??
         AntibodyInformation("BS10043", None, "Bioworld", "Anti-Rat"),  # Secondary antibody??
+
+        # 17;
         AntibodyInformation("OABB00472", None, "Aviva Systems Biology", "CD68"),
         AntibodyInformation("PA1080", None, "Boster Biological Technology", "VEGFA"),
         AntibodyInformation("BE0146", "RMP1-14", "Bio X Cell", "PDCD1"),
-
-        # 16;
         AntibodyInformation("A5862", None, "ABclonal", "XRCC5"),
+
+        # 21;
         AntibodyInformation("AF933", None, "R&D Systems", "ACE2"),
         AntibodyInformation("ALX-801-002-C100", "C219", "Enzo Life Sciences", "ABCB1"),
         AntibodyInformation("NB100-165", None, "Novus Biologicals", "XRCC3"),
